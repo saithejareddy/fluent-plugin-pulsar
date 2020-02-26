8d4d038d5f988ff6af46aef83ec8747de5677940
saitheja_juspay <saitheja.k@juspay.in>
2020-02-26 14:56:49 +0530

    Did Small changes

--- lib/fluent/plugin/lib/out_pulsar.rb
+++ lib/fluent/plugin/lib/out_pulsar.rb
@@ -1,15 +1,15 @@
 
 require "fluent/plugin/output"
-require 'pulsar_client/PulsarClient'
+require './pulsar_client/PulsarClient'
 module Fluent
   module Plugin
     class PulsarOutput < Fluent::Plugin::Output
       Fluent::Plugin.register_input('out_pulsar', self)
       # config Parameters
       desc 'The port of pulsar.'
-      config_param :pulsar_port, :integer, default: 6650
+      config_param :pulsar_port, :integer, default: nil
       desc 'The host of pulsar.'
-      config_param :pulsar_host, :string, default: 'pulsar-proxy.pulsar'
+      config_param :pulsar_host, :string, default: nil
       desc 'The topic of pulsar.'
       config_param :pulsar_topic, :string, default: 'persistent://pulsar/logs/default-topic'
       desc 'The name of producer.'
@@ -18,37 +18,41 @@ module Fluent
       config_param :num_messages, :int, default: 1
       desc 'The number of retries'
       config_param :num_retry, :int, default: 1
+      desc ''
       config_section :parse do
         config_set_default :@type, 'out_pulsar'
       end
+
       def configure(conf)
         super
+      end
 
-      end
-      def start
-        super
-        client = Message::PulsarClient.new()
-        client.connect(@pulsar_host, @pulsar_port)
-        log.info "Connected to pulsar brokers successfully"
-      end
-      def process(tag,es)
-        es.each do |time,record|
-          log.debug "Producing records to #{@pulsar_topic}"
-          send_to_pulsar(record)
+      def refresh_brokers(raise_error = true)
+        begin
+          if @pulsar_host != nil && @pulsar_port != nil
+            @producer = Message::PulsarClient.new()
+            @producer.connect(@pulsar_host, @pulsar_port)
+            log.info "Connected to pulsar brokers successfully"
+          end
+        rescue Exception => e
+          if raise_error
+            raise e
+          else
+            log.error e
+          end
         end
       end
-      def send_to_pulsar(record)
-        retry_count = 0
-        if client.send(@pulsar_topic, @pulsar_producer, @num_messages, record) == true
-          log.info "Successfully sent a record"
-        else
-          log.warn "Failed to send, retrying.."
-          retry_count += 1
-          if (retry_count <= @num_retry)
-            send_to_pulsar(record)
-          else
-            log.error "retry limit exceeded, dropping record.."
-          end
+
+      def start
+        super
+        refresh_brokers
+      end
+
+      def proces(tag,es)
+          es.each do |time, record|
+            log.debug "Publishing records to #{@pulsar_topic}"
+            @producer.send(@pulsar_topic,@pulsar_producer,@num_messages,record)
+            log.debug "Successfully sent a record to #{@pulsar_topic}"
         end
       end
     end
